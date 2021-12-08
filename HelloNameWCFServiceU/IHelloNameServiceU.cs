a3d992b84a2001b37dd474dddc375ea29c59d57c
URVASHI <URVASHI@DESKTOP-NJLCBTH>
2021-12-08 15:17:29 -0500

    Add project files.

---
+++ HelloNameWCFServiceU/IHelloNameServiceU.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloNameWCFServiceU
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IHelloNameServiceU" in both code and config file together.
+    [ServiceContract]
+    public interface IHelloNameServiceU
+    {
+        [OperationContract]
+        string HelloName(string name);
+    }
+}
---
+++ WCFHostHelloU/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFHostHelloU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(ServiceHost host = new ServiceHost(typeof(HelloNameWCFServiceU.HelloNameServiceU)))
+            {
+                host.Open();
+                Console.WriteLine("Host Started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
